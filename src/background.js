34226d8b36212eecc9131517da072364613f8669
shadmansaleh <13149513+shadmansaleh@users.noreply.github.com>
2025-03-07 14:57:57 +0600

    restrict which pages simple pesticide can run on

--- src/background.js
+++ src/background.js
@@ -1,24 +1,37 @@
 chrome.action.onClicked.addListener(async (tab) => {
-  let storedData = await chrome.storage.local.get("enabled");
-  let enabled = storedData.enabled ?? false;
-  let newState = !enabled;
+  try {
+    if (
+      !tab.url ||
+      tab.url.startsWith("chrome://") ||
+      tab.url.startsWith("about://") ||
+      tab.url.startsWith("file://")
+    ) {
+      console.log("Simple Pesticide: Extension won't run on this page.");
+      return; // Prevent running the extension on invalid pages
+    }
+    let storedData = await chrome.storage.local.get("enabled");
+    let enabled = storedData.enabled ?? false;
+    let newState = !enabled;
 
-  await chrome.storage.local.set({ enabled: newState });
+    await chrome.storage.local.set({ enabled: newState });
 
-  if (newState) {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      files: ["src/content_borders.js"],
-    });
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      files: ["src/content_info.js"],
-    });
-  } else {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      func: disableFeatures,
-    });
+    if (newState) {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ["src/content_borders.js"],
+      });
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ["src/content_info.js"],
+      });
+    } else {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: disableFeatures,
+      });
+    }
+  } catch (err) {
+    console.error("Simple Pesticide: Error toggling extension ", err);
   }
 });
 
